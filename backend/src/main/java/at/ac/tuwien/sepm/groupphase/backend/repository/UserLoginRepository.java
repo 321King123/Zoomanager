194e1a121089d706b962676fa47408b8e1f18c5f
zhsofia <sofia.zholubak@gmail.com>
2020-05-10 12:49:19 +0200

    [6.6] User DAO persistence

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserLoginRepository.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.sepm.groupphase.backend.repository;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLoginRepository extends JpaRepository<User, String> {
+
+    /**
+     * Finds a user with a specific username
+     *
+     * @param username of the user to be found
+     * @return user with the corresponding username
+     */
+    User findUserByUsername(String username);
+
+}
