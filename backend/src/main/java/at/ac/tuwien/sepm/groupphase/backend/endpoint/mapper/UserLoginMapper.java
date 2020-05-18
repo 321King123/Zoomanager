fdeab882d28fc9e4687d6e3879154b2a4e965805
Andreas Krystallidis <andreas.krystallidis@gmx.at>
2020-05-18 18:28:59 +0200

    [General fix] Testing does work now changed log level of application-test.yml to ERROR because otherwise the Tests dont work

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/UserLoginMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/UserLoginMapper.java
@@ -4,8 +4,9 @@ import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.EmployeeDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserLoginDto;
 import at.ac.tuwien.sepm.groupphase.backend.entity.UserLogin;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper
+@Mapper(unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserLoginMapper {
 
     UserLogin userLoginDtoToUserLogin(UserLoginDto userLoginDto);
