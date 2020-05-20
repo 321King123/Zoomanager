6d06652ff7d32b0cd2ef5c4bc9abf032102d8941
Sofia <sofia.zholubak@gmail.com>
2020-05-20 22:44:01 +0200

    [2.4] animal deletion frontend

--- frontend/src/app/components/animal/animal.component.ts
+++ frontend/src/app/components/animal/animal.component.ts
@@ -20,7 +20,7 @@ export class AnimalComponent implements OnInit {
     this.animalCreationForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       species: ['', [Validators.required]],
-      publicInformation: ['', [Validators.required]],
+      publicInformation: [''],
       description: ['', [Validators.required]]
     });
   }
@@ -83,6 +83,17 @@ export class AnimalComponent implements OnInit {
       },
       error => {
         console.log('Failed to load all animals');
+        this.defaultServiceErrorHandling(error);
+      }
+    );
+  }
+
+  deleteAnimal(animal: Animal) {
+    this.animalService.deleteAnimal(animal).subscribe(
+      (res: any) => {
+        this.getAnimals();
+      },
+      error => {
         this.defaultServiceErrorHandling(error);
       }
     );
