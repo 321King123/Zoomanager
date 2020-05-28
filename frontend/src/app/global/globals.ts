7fe3171748936b386f5d1808acc12ba6498df07e
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2020-05-28 14:04:06 +0200

    [Better Errors] Debug Console Log

--- frontend/src/app/global/globals.ts
+++ frontend/src/app/global/globals.ts
@@ -11,3 +11,7 @@ export enum type {
   DOCTOR = 'DOCTOR',
   JANITOR = 'JANITOR'
 }
+export namespace Utilities {
+  const DEBUG_MODE = true;
+  export const DEBUG_LOG = DEBUG_MODE ? console.log.bind(console) : function () {};
+}
