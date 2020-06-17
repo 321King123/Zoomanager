cee1888fb24364b1c2e0250841e656fa2b61aeb4
Manuel <e11808259@student.tuwien.ac.at>
2020-06-17 12:45:15 +0200

    [1.6] TaskComments Service Layer methods

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/TaskCommentRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/TaskCommentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<TaskComment, Long> {
+public interface TaskCommentRepository extends JpaRepository<TaskComment, Long> {
 
     List<TaskComment> findAllByTask(Task task);
 }
